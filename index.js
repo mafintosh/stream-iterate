3ed3d3b9562003a2d4771dd6ce59decf38257936
Mathias Buus <mathiasbuus@gmail.com>
2015-04-08 11:42:16 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+module.exports = function (stream) {
+  var ended = false
+  var data = null
+  var err = null
+  var destroyed = false
+  var fn = null
+
+  var consume = function (e) {
+    if (e && stream.destroy) {
+      destroyed = true
+      stream.destroy(e)
+      return
+    }
+
+    data = null
+    err = null
+  }
+
+  var onresult = function () {
+    if (!fn) return
+    var tmp = fn
+    fn = undefined
+    tmp(err, data, consume)
+  }
+
+  var update = function () {
+    if (!fn) return
+    data = stream.read()
+    if (data === null && !ended) return
+    onresult()
+  }
+
+  var onend = function () {
+    ended = true
+    onresult()
+  }
+
+  stream.on('readable', update)
+
+  stream.on('error', function (e) {
+    err = e
+    onresult()
+  })
+
+  stream.on('close', function () {
+    if (stream._readableState.ended) return
+    onend()
+  })
+
+  stream.on('end', onend)
+
+  return function (callback) {
+    if (destroyed) return
+    if (err) return callback(err, null, consume)
+    if (data) return callback(null, data, consume)
+    if (ended) return callback(null, null, consume)
+    fn = callback
+    update()
+  }
+}
